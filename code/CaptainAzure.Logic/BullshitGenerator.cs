dd4ce1ef7f6d860ee2a23331e90eea4941b74479
Peter Perov <peterperov@gmail.com>
2021-05-08 14:20:01 +0100

    initial

---
+++ code/CaptainAzure.Logic/BullshitGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptainAzure.Logic
+{
+    public class BullshitGenerator
+    {
+
+        static List<string> Adjectives = new List<string>(); 
+        static List<string> Verbs = new List<string>(); 
+        static List<string> Nouns = new List<string>(); 
+
+
+        static BullshitGenerator()
+        {
+
+            // lock later. 
+
+
+        }
+
+
+
+    }
+}
